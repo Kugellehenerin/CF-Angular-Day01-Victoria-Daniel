0be3b5716fc5345ef2647f0f59696f508b10c45d
Victoria Kahr <victoria.kahr@icloud.com>
2020-10-19 14:43:04 +0200

    basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
+
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import  { Routes, RouterModule } from '@angular/router' ;
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+import { HomePageComponent} from './home-page/home-page.component';
+
+import { AboutPageComponent} from  './about-page/about-page.component';
+
+
+const routes: Routes = [
+
+{
+
+        path: "",component: HomePageComponent
+
+},
+
+{
+
+        path:"about",component: AboutPageComponent
+
+}
+
+];
+
+
+@ NgModule({
+
+ imports: [RouterModule.forRoot(routes)
+
+         ],
+
+ exports: [RouterModule]
+
 })
+
 export class AppRoutingModule { }
